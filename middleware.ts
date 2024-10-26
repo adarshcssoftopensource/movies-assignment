8f6190f93ee00b614128b9468a677e8bedf376ff
adasrhcssoft <opensourcetech47@gmail.com>
2024-10-26 10:11:06 +0530

    Fix lint

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/login") {
+    return NextResponse.next();
+  }
   return NextResponse.redirect(new URL("/login", request.url));
 }
 
